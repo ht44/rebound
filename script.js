e32de57a9fbd5c0c5e4888d3b994b1750e791a94
Hayden Turek <haydenturek@gmail.com>
2017-06-15 16:59:47 -0500

    handle req res

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ const rgbValues = [
 ];
 
 class Player {
-  constructor(coordinates, isOffense, isShooter) {
-    this.coordinates = coordinates;
+  constructor(x, y, isOffense, isShooter) {
+    this.x = x;
+    this.y = y;
     this.isOffense = isOffense;
     this.isShooter = isShooter;
   }
@@ -44,15 +45,16 @@ class Player {
 const trigger = document.getElementById('run');
 const clear = document.getElementById('clear');
 const container = document.getElementById('container');
-const court = d3.select('#container').append('svg')
-                                //  .style('background-image', 'url(http://robslink.com/SAS/democd54/nba_court_dimensions.jpg)')
-                                   .attr('width', 500)
-                                   .attr('height',  470)
-                                   .style('border', '3px outset grey')
-                                   .style('background-size', 'cover')
-                                   .style('box-shadow', '4px 6px 33px 0px rgba(0,0,0,0.75)')
-                                   .on('click', placeOffender)
-                                   .on('contextmenu', placeDefender);
+const court = d3.select('#container')
+                .append('svg')
+                // .style('background-image', 'url(http://robslink.com/SAS/democd54/nba_court_dimensions.jpg)')
+                // .style('background-size', 'cover')
+                .attr('width', 500)
+                .attr('height',  470)
+                .style('border', '3px outset grey')
+                .style('box-shadow', '4px 6px 33px 0px rgba(0,0,0,0.75)')
+                .on('click', placeOffender)
+                .on('contextmenu', placeDefender);
 
 const hoop = court.append('circle')
                   .attr('cx', 250)
@@ -82,7 +84,7 @@ function placeOffender() {
 
     offense.push(player)
     payload.bench.push(new Player(
-      [xy[0] / 10, xy[1] / 10], true, false
+      (xy[0] / 10),(xy[1] / 10), true, false
     ));
     offense[offense.length - 1].attr('id', payload.bench.length - 1);
 
@@ -90,7 +92,7 @@ function placeOffender() {
 
   if ((window.event.shiftKey && !shooter) ||
       (offense.length === 5 && !shooter)) {
-    offense[offense.length - 1].attr('fill', 'gold')
+    offense[offense.length - 1].attr('fill', 'green')
                                .attr('stroke', 'gold')
     payload.bench[payload.bench.length - 1].isShooter = true;
     shooter = true;
@@ -113,7 +115,7 @@ function placeDefender() {
 
     defense.push(player);
     payload.bench.push(new Player(
-      [xy[0] / 10, xy[1] / 10], false, false
+      (xy[0] / 10), (xy[1] / 10), false, false
     ));
     defense[defense.length - 1].attr('id', payload.bench.length - 1);
 
@@ -121,25 +123,35 @@ function placeDefender() {
 }
 
 function logPayload() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://10.8.81.4:9099/predict');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        let response = JSON.parse(xhr.response);
+        offense.forEach((offender, i) => {
+          offender.property('probability', response[offender.attr('id')].probability);
+        });
 
-  offense.forEach((offender, i) => {
-    offender.property('prob', response.data[offender.attr('id')].prob);
-  });
+        defense.forEach((defender, i) => {
+          defender.property('probability', response[defender.attr('id')].probability);
+        });
 
-  defense.forEach((defender, i) => {
-    defender.property('prob', response.data[defender.attr('id')].prob);
-  });
-
-  offense.concat(defense)
-         .sort((a, b) => a.property('prob') - b.property('prob'))
-         .forEach((man, index) => {
-            man.transition()
-               .duration(1000)
-               .ease(d3.easeLinear)
-               .attr('fill', rgbValues[index])
-               .attr('cx', response.data[man.attr('id')].x)
-               .attr('cy', response.data[man.attr('id')].y)
-          });
+        offense.concat(defense)
+        .sort((a, b) => a.property('probability') - b.property('probability'))
+        .forEach((man, index) => {
+          man.transition()
+          .duration(1000)
+          .ease(d3.easeLinear)
+          .attr('fill', rgbValues[index])
+          .attr('cx', response[man.attr('id')].newx * 10)
+          .attr('cy', response[man.attr('id')].newy * 10)
+        });
+      }
+    }
+  }
+  xhr.send(JSON.stringify(payload));
+  // console.log(JSON.stringify(payload));
 }
 
 function restore() {
