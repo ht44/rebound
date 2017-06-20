deb432713cc38eeda72017eea850660af121fad4
Hayden Turek <haydenturek@gmail.com>
2017-06-19 20:57:05 -0500

    cleaned

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ const payload = {
   bench: new Array()
 }
 
-const response = [
-  {newx: 45, newy: 40, probability: 0.8},
-  {newx: 15, newy: 15, probability: 0.1},
-  {newx: 15, newy: 5, probability: 0.2},
-  {newx: 11, newy: 33, probability: 0.9},
-  {newx: 40, newy: 5, probability: 0.3},
-  {newx: 10, newy: 5, probability: 0.4},
-  {newx: 19, newy: 7, probability: 0.7},
-  {newx: 14, newy: 23, probability: 0.5},
-  {newx: 37, newy: 2, probability: 0.99},
-  {newx: 26, newy: 18, probability: 0.6}
-];
-
 const rgbValues = [
   'rgb(25, 0, 255)',
   'rgb(50, 0, 225)',
@@ -47,7 +34,6 @@ const clear = document.getElementById('clear');
 const container = document.getElementById('container');
 const court = d3.select('#container')
                 .append('svg')
-                // .style('background-image', 'url(svgs/ultimate.svg), url(images/5965362680_1e541c0e95.jpg)')
                 .style('background-image', 'url(svgs/ultimate.svg)')
                 .style('background-size', 'cover')
                 .style('background-color', 'rgb(47, 71, 62)')
@@ -58,28 +44,15 @@ const court = d3.select('#container')
                 .attr('height',  470)
                 .style('border', '3px outset grey')
                 .style('box-shadow', '4px 6px 33px 0px rgba(0,0,0,0.75)')
-                // .style('box-shadow', '2px 2px 23px 7px rgba(0,0,0,0.75)')
                 .on('click', placeOffender)
                 .on('contextmenu', placeDefender);
 
-// const hoop = court.append('circle')
-//                   .attr('cx', 250)
-//                   .attr('cy', 416.5)
-//                   .attr('r', 7.5)
-//                   .attr('stroke', 'yellow');
-
-
-trigger.addEventListener('click', logPayload);
+trigger.addEventListener('click', run);
 clear.addEventListener('click', restore);
 
-// implementation /////////////////////////////////////////////////////////////
-
 function placeOffender() {
-
   let xy = d3.mouse(this);
-
   if (offense.length < 5) {
-
     let player = court.append('circle')
                       .attr('class', 'player')
                       .attr('cx', xy[0])
@@ -89,15 +62,12 @@ function placeOffender() {
                       .attr('stroke', 'black')
                       .attr('stroke-width', '3')
                       .property('isOffense', true)
-                      // .on("mouseout", handleMouseOut);
-
     offense.push(player)
     payload.bench.push(new Player(
       (xy[0] / 10),(xy[1] / 10), true, false
     ));
     offense[offense.length - 1].attr('id', payload.bench.length - 1);
     offense[offense.length - 1].property('xid', offense.length);
-
     let label = court.append('g')
                      .attr('dx', xy[0])
                      .attr('dy', xy[1])
@@ -107,23 +77,19 @@ function placeOffender() {
                      .attr('y', xy[1])
                      .attr("dx", function(d){return -6})
                      .attr("dy", function(d){return 4})
-
   }
-
   if ((window.event.shiftKey && !shooter) ||
       (offense.length === 5 && !shooter)) {
     offense[offense.length - 1].attr('stroke', 'gold')
     payload.bench[payload.bench.length - 1].isShooter = true;
     shooter = true;
   }
-//
 }
 
 function placeDefender() {
   d3.event.preventDefault();
   let xy = d3.mouse(this);
   if (defense.length < 5) {
-
     let player = court.append('circle')
                       .attr('class', 'player')
                       .attr('cx', xy[0])
@@ -132,7 +98,6 @@ function placeDefender() {
                       .attr('fill', 'white')
                       .attr('stroke', 'black')
                       .attr('stroke-width', '3');
-
     defense.push(player);
     payload.bench.push(new Player(
       (xy[0] / 10), (xy[1] / 10), false, false
@@ -151,14 +116,14 @@ function placeDefender() {
   }
 }
 
-function logPayload() {
-  // let xhr = new XMLHttpRequest();
-  // xhr.open('POST', 'http://10.8.81.4:9099/predict');
-  // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  // xhr.onreadystatechange = () => {
-  //   if (xhr.readyState == 4) {
-  //     if (xhr.status == 200) {
-  //       let response = JSON.parse(xhr.response);
+function run() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://34.197.45.92:8080/predict');
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        let response = JSON.parse(xhr.response);
         offense.forEach((offender, i) => {
           offender.property('probability', response[offender.attr('id')].probability);
         });
@@ -170,9 +135,6 @@ function logPayload() {
         offense.concat(defense)
         .sort((a, b) => a.property('probability') - b.property('probability'))
         .forEach((man, index) => {
-          // let bVal = Math.floor(man.property('probability') * 255);
-          // let rVal = Math.floor((1 - man.property('probability')) * 255);
-          // let newColor = `rgb(${rVal}, 0, ${bVal})`;
           if (index === 9) {
             man.attr('stroke', 'blue')
           }
@@ -183,12 +145,11 @@ function logPayload() {
           .attr('cx', response[man.attr('id')].newx * 10)
           .attr('cy', response[man.attr('id')].newy * 10)
         });
-  //     }
-  //   }
-  // };
+      }
+    }
+  };
   d3.selectAll('circle').on("mouseover", handleMouseOver)
-  console.log(JSON.stringify(payload));
-  // xhr.send(JSON.stringify(payload));
+  xhr.send(JSON.stringify(payload));
 }
 
 function handleMouseOver() {
@@ -201,7 +162,6 @@ function handleMouseOver() {
     prefix = 'D'
   }
   banner.innerText = `${prefix}${suffix.toString()} | P = ${prob}`;
-  console.log(d3.select(this).property('isOffense'));
 }
 
 function restore() {
